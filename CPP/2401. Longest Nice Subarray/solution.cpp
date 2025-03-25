cdb29dd11fff4c634ad4a2d3c0278cf419341cd5
Sonit Bahl <sonitbahl@gmail.com>
2025-03-25 22:39:46 +0530

    cpp code for 2401

---
+++ CPP/2401. Longest Nice Subarray/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int longestNiceSubarray(vector<int>& nums) {
+            int left = 0, bitmask = 0, maxLen = 0;
+            
+            for (int right = 0; right < nums.size(); ++right) {
+                while ((bitmask & nums[right]) != 0) {
+                    bitmask ^= nums[left]; 
+                    left++;                 
+                }
+                bitmask |= nums[right];     
+                maxLen = max(maxLen, right - left + 1);
+            }
+            
+            return maxLen;
+        }
+    };
+    
