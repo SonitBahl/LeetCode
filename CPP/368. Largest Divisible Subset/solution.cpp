9109d2edf7609e3c9fd51944ec348dd2b1aac5c4
Sonit Bahl <sonitbahl@gmail.com>
2025-04-06 11:38:13 +0530

    added cpp sol for 368

---
+++ CPP/368. Largest Divisible Subset/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    public:
+        vector<int> largestDivisibleSubset(vector<int>& nums) {
+            int n = nums.size();
+            if (n == 0) return {};
+    
+            sort(nums.begin(), nums.end());
+            vector<int> dp(n, 1), prev(n, -1);
+            int maxSize = 1, maxIndex = 0;
+    
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        prev[i] = j;
+                    }
+                }
+                if (dp[i] > maxSize) {
+                    maxSize = dp[i];
+                    maxIndex = i;
+                }
+            }
+    
+            vector<int> result;
+            for (int i = maxIndex; i >= 0; i = prev[i]) {
+                result.push_back(nums[i]);
+                if (prev[i] == -1) break;
+            }
+    
+            reverse(result.begin(), result.end());
+            return result;
+        }
+    };
+    
